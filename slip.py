98e1cde53a93bc54a5985ede302085b1c3189035
ThePaladini <82978181+ThePaladini@users.noreply.github.com>
2021-10-04 21:38:16 -0300

    Update slip.py

--- slip.py
+++ slip.py
@@ -51,7 +51,7 @@ class Enlace:
         # TODO: Preencha aqui com o código para enviar o datagrama pela linha
         # serial, fazendo corretamente a delimitação de quadros e o escape de
         # sequências especiais, de acordo com o protocolo CamadaEnlace (RFC 1055).
-        datagrama = '0xc0' + datagrama + '0xc0'
+        datagrama = b'0xc0' + datagrama + b'0xc0'
         self.linha_serial.enviar(datagrama)
 
         pass
